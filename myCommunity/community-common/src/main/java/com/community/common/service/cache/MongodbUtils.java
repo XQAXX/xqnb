b1394ff00059f1e644eb1629211aae029370bbdf
XQHLH <455116383@qq.com>
2019-12-09 15:01:44 +0800

    项目搭建

--- myCommunity/community-common/src/main/java/com/community/common/service/cache/MongodbUtils.java
+++ myCommunity/community-common/src/main/java/com/community/common/service/cache/MongodbUtils.java
@@ -1,4 +1,4 @@
-package com.community.common.util;
+package com.community.common.service.cache;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
---
+++ myCommunity/community-upload/src/main/java/com/community/upload/controller/ImageUploadController.java
@@ -0,0 +1,12 @@
+package com.community.upload.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * 图片上传
+ * @author
+ */
+@Controller
+@RequestMapping("/image")
+public class ImageUploadController {
+
+}
